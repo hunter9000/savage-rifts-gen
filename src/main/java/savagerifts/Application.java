cb8d68e246c3c2b4d875df3d77944c4bde9c3a93
Hunter <hunter9000@gmail.com>
2016-10-27 01:07:59 -0400

    make swagger work again

--- src/main/java/savagerifts/Application.java
+++ src/main/java/savagerifts/Application.java
@@ -16,10 +16,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaRepositories(basePackages = "savagerifts.repository")
 @EntityScan(basePackages = "savagerifts")
 @SpringBootApplication
+@EnableSwagger2
 public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
@@ -67,8 +69,8 @@ public class Application extends WebMvcConfigurerAdapter {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("rest angular sample")
-                .description("API documentation")
+                .title("Savage Rifts Character Generator")
+                .description("")
                 .version("1.0")
                 .build();
     }
