a0fbc269d41424e54229d3a53274b6d183c5c4a7
Hunter <hunter9000@gmail.com>
2016-11-10 11:28:00 -0500

    race definitions

--- sql/15.sql
+++ sql/15.sql
@@ -106,17 +106,42 @@ SET @DRAGON_ID = LAST_INSERT_ID();
 
 
 INSERT INTO `framework_feature` (`framework`, `framework_feature_type`, `framework_ability_type`, `description`) VALUES
-(@CYBER_KNIGHT_ID, 'CYBER_ARMOR', 'BONUS', ''),
-(@CYBER_KNIGHT_ID, 'CYBERKINETIC_COMBAT', 'BONUS', ''),
-(@CYBER_KNIGHT_ID, 'FIRST_INTO_BATTLE', 'BONUS', ''),
-(@CYBER_KNIGHT_ID, 'INNER_LIGHT', 'BONUS', ''),
-(@CYBER_KNIGHT_ID, 'INTENSE_COMBAT_TRAINING', 'BONUS', ''),
-(@CYBER_KNIGHT_ID, 'MINOR_PSIONIC', 'BONUS', ''),
-(@CYBER_KNIGHT_ID, 'PSI_SWORD', 'BONUS', ''),
-(@CYBER_KNIGHT_ID, 'REVERED_PROTECTORS', 'BONUS', ''),
+(@CYBER_KNIGHT_ID, 'CK_CYBER_ARMOR', 'BONUS', 'As a free action, Cyber-Knights can summon an organic metal shell, granting +2 Toughness.'),
+(@CYBER_KNIGHT_ID, 'CK_CYBERKINETIC_COMBAT', 'BONUS', 'Any purely technology-based attacks, such as lasers, rail guns, and vibro-blades, suffer a −2 to hit a Cyber-Knight. This ability stacks with the deflection power.'),
+(@CYBER_KNIGHT_ID, 'CK_FIRST_INTO_BATTLE', 'BONUS', 'Cyber-Knights begin with +2 Pace and a d10 running die. If they take the Fleet-Footed Edge, the Pace bonuses stack and the running die becomes a d12.'),
+(@CYBER_KNIGHT_ID, 'CK_INNER_LIGHT', 'BONUS', 'Cyber-Knights begin with the Champion Edge (see Savage Worlds).'),
+(@CYBER_KNIGHT_ID, 'CK_INTENSE_COMBAT_TRAINING', 'BONUS', 'A Cyber- Knight begins play with Fighting d8 and two Combat Edges (meeting all requirements except Rank).'),
+(@CYBER_KNIGHT_ID, 'CK_MINOR_PSIONIC', 'BONUS', 'Cyber- Knights have Arcane Background (Psionics), 10 ISP, three powers, and Psionics d6. They have access to the following powers: armor, barrier, bolt, boost Trait*, confusion, darksight, deflection*, environmental protection, havoc, healing*, pummel, quickness, smite*, speak language, speed*, succor, warrior’s gift. Powers marked with an asterisk (*) are usable only on the Cyber-Knight but activate as a free action, inflicting no multiple action penalty (a Cyber-Knight with Master Psionic can use mass healing but not as a free action). All other aspects of AB (Psionics) remain the same.'),
+(@CYBER_KNIGHT_ID, 'CK_PSI_SWORD', 'BONUS', 'As a free action, Cyber- Knights can summon forth a blade of spirit and will. It may have any appearance but it does not change the effects. The Psi-Sword does Strength + Spirit × 2 damage with AP 6 (and no Strength minimum). For example, a Cyber-Knight with Strength d8 and Spirit d10 does 1d8+2d10 melee damage, AP 6, with his Psi-Sword. Alternately, the Cyber-Knight may split his Psi- Sword into two blades, one for each hand, doing the same damage, but each blade only gaining AP 2. For 2 ISP, the Cyber-Knight can make the blade inflict Mega Damage for as long as the blade is manifested.'),
+(@CYBER_KNIGHT_ID, 'CK_REVERED_PROTECTORS', 'BONUS', 'All Cyber-Knights enjoy +2 Charisma when dealing with those who know and love them as guardians of life and freedom. '),
 
-(@CYBER_KNIGHT_ID, 'CODE_OF_HONOR', 'COMPLICATION', ''),
-(@CYBER_KNIGHT_ID, 'CYBERNETICS', 'COMPLICATION', ''),
+(@CYBER_KNIGHT_ID, 'CK_CODE_OF_HONOR', 'COMPLICATION', 'A Cyber-Knight must adhere to the codes and philosophies of the Order, or he loses some or all of his powers, depending on the degree of transgression. Whether this is a function of the special nanotech treatment he undergoes or a loss of confidence damaging his psyche in some way is a matter of some debate among scholars. Regardless, the Game Master must determine the extent of a Cyber-Knight’s transgressions and assess a penalty accordingly. Minor Transgression: −1 to all Psionics rolls until he performs a noble or heroic deed to atone. Major Transgression: −2 to all Psionics rolls, and all abilities under Cyberkinetic Combat and Inner Light cease to function until he undertakes some kind of personal (or perhaps spiritually guided) quest to atone. His Psi-Sword is also reduced to doing only his Str+Spirit in damage, and all AP values are reduced by 2. Mortal Transgression: The Cyber- Knight commits an action completely opposed to the code. All Cyber- Knight abilities are lost. The character may regain his abilities by fulfilling a significant quest (determined by the Game Master). Should the character commit another Mortal Transgression before regaining his Cyber-Knight status, his spirit is corrupted. The character regains use of his abilities but becomes a Fallen Cyber-Knight (see Savage Foes of North America) under the Game Master’s control.'),
+(@CYBER_KNIGHT_ID, 'CK_CYBERNETICS', 'COMPLICATION', 'Cybernetics are not an option for Cyber-Knights, as such devices interfere with their nanotechnology.'),
+
+(@GLITTER_BOY_ID, 'GB_FULLY_TRAINED', 'BONUS', 'All Glitter Boy pilots begin with the Power Armor Jock Edge.'),
+(@GLITTER_BOY_ID, 'GB_GLITTER_BOY_ARMOR', 'BONUS', 'Power Armor of the highest caliber (see sidebar)'),
+(@GLITTER_BOY_ID, 'GB_HEROIC_LEGACY', 'BONUS', 'A Glitter Boy pilot gets +2 Charisma. If a Glitter Boy is stolen, word passes quickly and the thief is targeted by Glitter Boy pilots across North America.'),
+(@GLITTER_BOY_ID, 'GB_MANY_HAVE_FALLEN', 'BONUS', 'Numerous Glitter Boys fell to defend the world, leaving plentiful salvageable parts. Serious repairs cost 250,000 credits and Severe ones 500,000 (see page 122).'),
+
+(@GLITTER_BOY_ID, 'GB_BIG_AND_SHINY', 'COMPLICATION', 'Glitter Boys are immediately recognizable and often a priority target for heavy weaponry. They suffer −8 to Stealth rolls due to their glittering armored coating. At 10 feet tall and Size 3, Glitter Boys are big targets and man-sized opponents gain +2 to hit them (see page 84).'),
+(@GLITTER_BOY_ID, 'GB_CLOSED_OFF', 'COMPLICATION', 'Sealed away from the natural and spiritual worlds, Glitter Boy pilots cannot use magic or psionics while in the suit. Such powers used by others can still affect them.'),
+(@GLITTER_BOY_ID, 'GB_DIGGING_IN', 'COMPLICATION', 'Activating/deactivating the stabilization system is an action. Firing the Boom Gun without these measures knocks the Glitter Boy 2d6” back and prone, leaving the pilot Shaken. Once dug in, the Glitter Boy cannot move and can only fire to the front and sides (180 degrees). Digging in makes the Glitter Boy an easier target, attackers gain a +2 to attack rolls against the suit while dug in.'),
+(@GLITTER_BOY_ID, 'GB_ENEMIES', 'COMPLICATION', 'The Coalition States consider all Glitter Boys enemies of the state. Few within the True Federation of Magic consider them friends either.'),
+(@GLITTER_BOY_ID, 'GB_HIGHLY_TECHNICAL_MACHINERY', 'COMPLICATION', 'Like all devices, armor, vehicles, and weapons, Glitter Boy systems are subject to the Technical Difficulties Setting Rule (see page 122).'),
+(@GLITTER_BOY_ID, 'GB_SONIC_BOOM', 'COMPLICATION', 'Firing a Boom Gun causes a sonic boom affecting everyone (except the pilot) within a Large Burst Template. A failure on a Vigor roll −2 inflicts Hard of Hearing (Major) for 3d6 minutes and makes the character Shaken. If he fails with a 1 on the Vigor die, he is Incapacitated and makes a Vigor roll each round to become conscious but Shaken. Anyone in environmentally sealed armor gains +2 to the roll, as do characters with Hard of Hearing (Minor). Characters with the Major version are immune!'),
+(@GLITTER_BOY_ID, 'GB_STAND_BY_TO_FIRE', 'COMPLICATION', 'A Glitter Boy cannot move and fire its Boom Gun on the same round. This is why some carry other firearms as alternatives for when they need to move and shoot.'),
+
+(@JUICER_ID, 'JC_INTERNAL_REPAIR_SYSTEM', 'The biocomp system managing the drugs and chemical processes of the Juicer’s body also works overtime to ensure his continued health and good repair. The Juicer gains Slow Regeneration (a natural healing roll once per day). As well, there’s a continuously replenishing supply of nanites and chemical concoctions available for “emergency repairs.” As an action, the Juicer can makea natural healing roll at +2 to heal wounds; this costs one charge. The system holds a maximum of three charges and requires eight hours to recover one charge.'),
+(@JUICER_ID, 'JC_SUPER_ENDURANCE', 'Juicers begin with +2 Vigor die types, with no Trait maximum. They require only half the normal amount of sleep, and gain +2 on all Fatigue checks.'),
+(@JUICER_ID, 'JC_SUPER_REFLEXES', 'Juicers begin with +2 Agility die types, with no Trait maximum. They have Uncanny Reflexes, granting them −2 to be hit by all attacks. Finally, they begin with the Quick Edge.'),
+(@JUICER_ID, 'JC_SUPER_SPEED', 'Juicers cover enormous ground quickly, doubling their base Pace. They also have the Fleet-Footed Edge (meaning they have Pace of 16” with a d10 running die).'),
+(@JUICER_ID, 'JC_SUPER_STRENGTH', 'Juicers begin with +2 Strength die types, with no Trait maximum. They also begin with the Brawny Edge.'),
+
+(@JUICER_ID, 'JC_CYBERNETICS', 'Although it is possible to add cybernetics to the Juicer’s already hacked biology, there is a severe drawback: each point of Strain removes a point of Burn!'),
+(@JUICER_ID, 'JC_DEATH_WISH', 'The Juicer knows he’s going to die, and he wants to go out magnificently, having done something folks remember and are inspired by. He has the Death Wish Hindrance (Minor).'),
+(@JUICER_ID, 'JC_DRUG_INDUCED_EUPHORIA_TRANQUILITY', 'In order to keep the Juicer from burning out over a matter of days, the bio-comp constantly supplies a combination of psychotropic drugs and chemical inhibitors to keep the subject happy and calm. This results in a Juicer living in a constant state somewhere between mildly ecstatic and stoned, which causes −1 to all Smarts or Smarts-linked skill rolls in non-combat or low stress situations. On the first round of any combat, he doesn’t gain the benefits of Uncanny Reflexes or Quick.'),
+(@JUICER_ID, 'JC_PSIONICS_AND_MAGIC', 'The chemical cocktail inside the Juicer makes it impossible for him to use magic or psionics in any way.'),
+
 
 
 INSERT INTO `sql_files` (`sql_file_name`, `run_date`) VALUES ('15.sql', NOW());
--- src/main/java/savagerifts/model/framework/FrameworkFeatureType.java
+++ src/main/java/savagerifts/model/framework/FrameworkFeatureType.java
@@ -35,35 +35,50 @@ public enum FrameworkFeatureType {
 	DISTINCTIVE_APPEARANCE("Distinctive Appearance"),
 	MAGIC("Magic"),
 	NEEDS_ACTION("Needs Action"),
-	UNSTABLE_PSYCHO("Unstable Psycho");
+	UNSTABLE_PSYCHO("Unstable Psycho"),
 
 	// cyber-knight
-	CYBER_ARMOR("Cyber-Armor"),
-	CYBERKINETIC_COMBAT("Cyberkinetic Combat"),
-	FIRST_INTO_BATTLE("First Into Battle"),
-	INNER_LIGHT("Inner Light"),
-	INTENSE_COMBAT_TRAINING("Intense Combat Training"),
-	MINOR_PSIONIC("Minor Psionic"),
-	PSI_SWORD("Psi-Sword"),
-	REVERED_PROTECTORS("Revered Protectors"),
-	
-	CODE_OF_HONOR("Code of Honor"),
-	CYBERNETICS("Cybernetics");
+	CK_CYBER_ARMOR("Cyber-Armor"),
+	CK_CYBERKINETIC_COMBAT("Cyberkinetic Combat"),
+	CK_FIRST_INTO_BATTLE("First Into Battle"),
+	CK_INNER_LIGHT("Inner Light"),
+	CK_INTENSE_COMBAT_TRAINING("Intense Combat Training"),
+	CK_MINOR_PSIONIC("Minor Psionic"),
+	CK_PSI_SWORD("Psi-Sword"),
+	CK_REVERED_PROTECTORS("Revered Protectors"),
+
+	CK_CODE_OF_HONOR("Code of Honor"),
+	CK_CYBERNETICS("Cybernetics"),
 	
 	// glitter boy
-	FULLY_TRAINED("Fully Trained"),
-	GLITTER_BOY_ARMOR("Glitter Boy Armor"),
-	HEROIC_LEGACY("Heroic Legacy"),
-	MANY_HAVE_FALLEN("Many Have Fallen"),
-	
-	BIG_AND_SHINY("Big and Shiny"),
-	CLOSED_OFF("Closed Off"),
-	DIGGING_IN("Digging In"),
-	ENEMIES,("Enemies"),
-	HIGHLY_TECHNICAL_MACHINERY("Highly Technical Machinery"),
-	SONIC_BOOM("Sonic Boom"),
-	STAND_BY_TO_FIRE("Stand By to Fire"),
-	
+	GB_FULLY_TRAINED("Fully Trained"),
+	GB_GLITTER_BOY_ARMOR("Glitter Boy Armor"),
+	GB_HEROIC_LEGACY("Heroic Legacy"),
+	GB_MANY_HAVE_FALLEN("Many Have Fallen"),
+
+	GB_BIG_AND_SHINY("Big and Shiny"),
+	GB_CLOSED_OFF("Closed Off"),
+	GB_DIGGING_IN("Digging In"),
+	GB_ENEMIES("Enemies"),
+	GB_HIGHLY_TECHNICAL_MACHINERY("Highly Technical Machinery"),
+	GB_SONIC_BOOM("Sonic Boom"),
+	GB_STAND_BY_TO_FIRE("Stand By to Fire"),
+
+	// juicer
+	JC_INTERNAL_REPAIR_SYSTEM("Internal Repair System"),
+	JC_SUPER_ENDURANCE("Super Endurance"),
+	JC_SUPER_REFLEXES("Super Reflexes"),
+	JC_SUPER_SPEED("Super Speed"),
+	JC_SUPER_STRENGTH("Super Strength"),
+
+	JC_CYBERNETICS("Cybernetics"),
+	JC_DEATH_WISH("Death Wish"),
+	JC_DRUG_INDUCED_EUPHORIA_TRANQUILITY("Drug-induced Euphoria/Tranquility"),
+	JC_PSIONICS_AND_MAGIC("Psionics and Magic"),
+
+
+
+
 	;
 	
 	private String displayName;
