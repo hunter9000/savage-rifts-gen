c574c44c9c5662f2ab2ff98b52ede7770dafc204
Hunter Davis <hunter9000@gmail.com>
2017-02-16 16:48:07 -0500

    no message

--- src/main/java/savagerifts/Application.java
+++ src/main/java/savagerifts/Application.java
@@ -1,16 +1,17 @@
 package savagerifts;
 
-import savagerifts.interceptor.JWTInterceptor;
-import savagerifts.interceptor.RolePermissionInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import savagerifts.interceptor.JWTInterceptor;
+import savagerifts.interceptor.RolePermissionInterceptor;
 import savagerifts.interceptor.SheetOwnerInterceptor;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -21,6 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaRepositories(basePackages = "savagerifts.repository")
 @EntityScan(basePackages = "savagerifts")
 @SpringBootApplication
+@EnableWebSecurity
 @EnableSwagger2
 public class Application extends WebMvcConfigurerAdapter {
 
