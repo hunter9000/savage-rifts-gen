c21f4a6cf4691a22243a888015c7e6509e2fe5e8
Hunter Davis <hunter9000@gmail.com>
2017-05-16 16:13:00 -0400

    spring upgrade

--- src/main/java/savagerifts/Application.java
+++ src/main/java/savagerifts/Application.java
@@ -1,16 +1,16 @@
 package savagerifts;
 
-import savagerifts.interceptor.JWTInterceptor;
-import savagerifts.interceptor.RolePermissionInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import savagerifts.interceptor.JWTInterceptor;
+import savagerifts.interceptor.RolePermissionInterceptor;
 import savagerifts.interceptor.SheetOwnerInterceptor;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
